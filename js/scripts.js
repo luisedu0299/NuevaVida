19f5aa9b3b6729119a4eca82bc188c8862bb7667
luised0299 <luisedu0299@gmail.com>
2022-05-05 17:51:15 -0500

    Subir Proyecto

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#lista').DataTable();
+  });
+
+  $(document).ready(function() {
+    $('#lista').DataTable( {
+        "language": {
+            "lengthMenu": "Display _MENU_ records per page",
+            "zeroRecords": "Nothing found - sorry",
+            "info": "Showing page _PAGE_ of _PAGES_",
+            "infoEmpty": "No records available",
+            "infoFiltered": "(filtered from _MAX_ total records)"
+        }
+    } );
+} );
